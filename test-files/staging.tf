35c6b86049ea6483dcfddc01b7fc325d14a9f212
PedroSilvaBK <105646819+PedroSilvaBK@users.noreply.github.com>
2025-01-11 20:42:12 +0100

    Update staging.tf

--- test-files/staging.tf
+++ test-files/staging.tf
@@ -35,6 +35,11 @@ resource "google_compute_instance" "performance_test_vm" {
     apt-get update
     apt-get install -y wget curl git build-essential
     # Install any additional dependencies here (e.g., k6, Locust)
+    gpg -k
+    gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+    echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
+    apt-get update
+    apt-get install k6
   EOT
 
   tags = ["performance-test"]
