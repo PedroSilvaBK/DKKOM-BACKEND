fa166e96959c86c241523e5579131fe1b0c94b9f
PedroSilvaBK <105646819+PedroSilvaBK@users.noreply.github.com>
2025-01-14 21:39:08 +0100

    sdf

--- voice-video-service/main.go
+++ voice-video-service/main.go
@@ -212,9 +212,9 @@ func createPeerConnection(w http.ResponseWriter, r *http.Request) {
 			//	URLs: []string{"stun:stun.l.google.com:19302"},
 			//},
 			{
-				URLs:       []string{"turn:turn.dkkom.com:3478"}, // Replace with your TURN server IP/hostname
-				Username:   "username",                           // Your TURN server username
-				Credential: "password",                           // Your TURN server password
+				URLs:       []string{"turn:turn.dkkom.com:3478"},
+				Username:   "username",
+				Credential: "password",
 			},
 		},
 	})
@@ -396,8 +396,6 @@ func handleNewPeer(peer *Peer, roomId string) {
 		if i == nil {
 			return
 		}
-		// If you are serializing a candidate make sure to use ToJSON
-		// Using Marshal will result in errors around `sdpMid`
 
 		candidateResponse := &ICECandidateResponse{
 			Candidate: i.ToJSON(),
