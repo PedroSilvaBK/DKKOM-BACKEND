49e4284c2fcb0f6b9fe560bedf42353a672a00c2
PedroSilvaBK <105646819+PedroSilvaBK@users.noreply.github.com>
2025-01-17 13:05:39 +0100

    removing api gateway depedency setup

---
+++ Messaging Service/src/main/java/dcom/messaging_service/config/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package dcom.messaging_service.config.security;
+
+import dcom.cave_service.configuration.filter.TokenFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final TokenFilter tokenFilter;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
