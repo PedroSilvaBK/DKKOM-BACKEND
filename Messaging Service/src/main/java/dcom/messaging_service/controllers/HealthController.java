1e02c8b687b970a38faf5d9fc2b40e892bc7d9f3
PedroSilvaBK <105646819+PedroSilvaBK@users.noreply.github.com>
2025-01-17 17:56:17 +0100

    Update HealthController.java

--- Messaging Service/src/main/java/dcom/messaging_service/controllers/HealthController.java
+++ Messaging Service/src/main/java/dcom/messaging_service/controllers/HealthController.java
@@ -1,21 +1,9 @@
 package dcom.messaging_service.controllers;
 
-import dcom.messaging_service.business.Unauthorized;
-import dcom.messaging_service.business.create_message.CreateMessageUseCase;
-import dcom.messaging_service.business.export_user_messages.usecase.ExportUserMessagesUseCase;
-import dcom.messaging_service.business.get_messages.use_case.GetMessagesUseCase;
-import dcom.messaging_service.domain.requests.CreateMessageRequest;
-import dcom.messaging_service.domain.requests.GetMessagesRequest;
-import dcom.messaging_service.domain.responses.GetMessagesResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-import java.util.UUID;
 
 
 @RestController
