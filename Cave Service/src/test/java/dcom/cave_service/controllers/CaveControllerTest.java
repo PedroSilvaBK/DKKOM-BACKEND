9081ed18a8df51e420dbec45a9eb8069e500434e
PedroSilvaBK <105646819+PedroSilvaBK@users.noreply.github.com>
2025-01-20 00:20:02 +0100

    Update CaveControllerTest.java

--- Cave Service/src/test/java/dcom/cave_service/controllers/CaveControllerTest.java
+++ Cave Service/src/test/java/dcom/cave_service/controllers/CaveControllerTest.java
@@ -32,12 +32,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
